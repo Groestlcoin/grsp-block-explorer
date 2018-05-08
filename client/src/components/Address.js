1da539007f9eb7f21fb4d126e2141f10040754b9
Emmanuel Florent <emmanuel.florent@gmail.com>
2018-05-08 20:05:09 +0200

    initial commit

--- client/src/components/Address.js
+++ client/src/components/Address.js
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2018  Emmanuel Florent
+
+  This file is part of btpc-block-explorer.
+  https://github.com/emmanuel-florent/btcp-block-explorer
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import React from 'react'
 import { Jumbotron } from 'reactstrap'
 import { Spinner, spinnerService } from '@chevtek/react-spinners'
