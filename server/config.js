be2930aa6935ab2fec5e54c41f850103360ced09
Emmanuel Florent <emmanuel.florent@gmail.com>
2018-05-21 12:14:28 +0200

    removed unused file

--- server/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    electrumAddress : '192.168.1.49',
-    electrumPort : '51002',
-    electrumProtocol: 'tls',
-    bitcoindUrl: 'http://bitcoin:mP7hR_3yaiFSHATTipGH0t4cKYD49UcDoawtejAgfQg=@192.168.1.49:18332'
-}
-
-module.exports = config
